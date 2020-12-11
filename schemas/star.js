f1afd6dac023c1eb89cbbf19d8faa78cf8801129
Carolina Rodriguez Torres <c.torres@cheil.com>
2020-12-11 10:07:20 +0100

    add star to schema, for check if api and cms are deployed

---
+++ schemas/star.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'star',
+  title: 'Star',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+    },
+  },
+}
